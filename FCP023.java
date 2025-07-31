b8e6872898fd12ea1dd7f5ffc12150ad3f55643a
XcelevatorAbinaya <abinaya.2502@xcelevate.org>
2025-07-31 13:01:17 +0530

    Add files via upload

---
+++ FCP023.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class FCP023{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+String word=sc.next();
+for (int i=0;i<=a;i++){
+System.out.println(word);
+}
+}
+}
